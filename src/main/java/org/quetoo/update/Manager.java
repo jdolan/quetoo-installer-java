f8c50132ee29b7e8d1b424d22c8ace0d3fa92c6a
Jay Dolan <jay@jaydolan.com>
2017-01-14 23:47:03 -0500

    Okay, React is powerful as shit.

--- src/main/java/org/quetoo/update/Manager.java
+++ src/main/java/org/quetoo/update/Manager.java
@@ -2,12 +2,8 @@ package org.quetoo.update;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.concurrent.Callable;
-import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
 import org.quetoo.update.aws.S3BucketSync;
@@ -25,7 +21,7 @@ public class Manager {
 	
 	private final Config config;
 
-	private final List<Sync> syncs;
+	private final Observable<Sync> syncs;
 	
 	private final Logger log = LoggerFactory.getLogger(getClass());
 	
@@ -37,25 +33,26 @@ public class Manager {
 	public Manager(final Config config) {
 		this.config = config;
 		
-		syncs = new ArrayList<>();
-		
-		syncs.add(new S3BucketSync.Builder()
+		syncs = Observable.fromArray(
+				
+			new S3BucketSync.Builder()
 				.withAmazonS3Client(config.getAmazonS3Client())
 				.withHttpClient(config.getHttpClient())
 				.withBucketName("quetoo")
 				.withPredicate(s -> s.getKey().startsWith(config.getArchHostPrefix()))
 				.withMapper(s -> new File(s.getKey().replace(config.getArchHostPrefix(), "")))
 				.withDestination(config.getDir())
-				.build());
-		
-		syncs.add(new S3BucketSync.Builder()
+				.build(),
+				
+			new S3BucketSync.Builder()
 				.withAmazonS3Client(config.getAmazonS3Client())
 				.withHttpClient(config.getHttpClient())
 				.withBucketName("quetoo-data")
 				.withPredicate(s -> true)
 				.withMapper(s -> new File(s.getKey()))
 				.withDestination(config.getShare())
-				.build());
+				.build()
+		);
 	}
 	
 	/**
@@ -63,28 +60,42 @@ public class Manager {
 	 * 
 	 * @param files The aggregate sync result.
 	 */
-	private void prune(Set<File> files) {
+	private void onComplete(Set<File> files) {
 		
 		if (config.getPrune()) {
-			FileUtils.listFiles(config.getDir(), null, true).stream().filter(file -> {;
-				return !files.contains(file);
-			}).forEach(file -> {
-				FileUtils.deleteQuietly(file);
+			FileUtils.listFiles(config.getDir(), null, true).stream().filter(f -> {;
+				return !files.contains(f);
+			}).forEach(f -> {
+				FileUtils.deleteQuietly(f);
 				
-				log.info("Removed {}", file);
+				log.info("Removed {}", f);
 			});
 		}
 	}
 	
-	private void onNext(final File file) {
+	/**
+	 * Modifies permissions on the newly synced File.
+	 * 
+	 * @param file The newly synced File.
+	 * 
+	 * @return The File.
+	 */
+	private File onSync(final File file) {
 		
 		if (file.getParentFile().equals(config.getBin())) {
 			file.setExecutable(true);
 		}
 		
 		log.info("Updated {}", file);
+		
+		return file;
 	}
 	
+	/**
+	 * Logs the given error.
+	 * 
+	 * @param t The Throwable error.
+	 */
 	private void onError(final Throwable t) {
 		log.error(t.getMessage(), t);
 	}
@@ -96,31 +107,9 @@ public class Manager {
 	 */
 	public void sync() throws IOException {
 		
-		Observable<File> files = Observable.merge(
-			syncs.stream().map(s -> safe(() -> s.sync())).collect(Collectors.toList())
-		);
-		
-		files.subscribe(this::onNext, this::onError);
-		
-		files.collectInto(new HashSet<File>(), (set, file) -> set.add(file)).subscribe(this::prune);
-	}
-	
-	/**
-	 * Invokes the callable, re-throwing any checked Exceptions as RuntimeExceptions.
-	 * 
-	 * @param <T> The Callable result type.
-	 * @param callable A Callable.
-	 * @return The Callable result.
-	 */
-	public static <T> T safe(Callable<T> callable) {
-		try {
-			return callable.call();
-		} catch (Throwable t) {
-			if (t instanceof RuntimeException) {
-				throw (RuntimeException) t;
-			} else {
-				throw new RuntimeException(t);
-			}
-		}
+		Observable.merge(syncs.map(Sync::sync))
+			.map(this::onSync)
+			.collectInto(new HashSet<File>(), (set, file) -> set.add(file))
+			.subscribe(this::onComplete, this::onError);		
 	}
 }
