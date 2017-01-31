3b20b6aa6154bff7d8b077f3ff433d2fda3a7657
Paril <paril@alteredsoftworks.com>
2017-01-31 11:14:30 -0500

    Encode the remote bucket key for URI

--- src/main/java/org/quetoo/installer/aws/S3BucketSync.java
+++ src/main/java/org/quetoo/installer/aws/S3BucketSync.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLEncoder;
 import java.util.function.Function;
 
 import org.apache.commons.io.FileUtils;
@@ -112,7 +113,7 @@ public class S3BucketSync implements Sync {
 	 */
 	private <T> T executeHttpRequest(final String path, ResponseHandler<T> handler) throws IOException {
 
-		final String uri = "http://" + bucketName + ".s3.amazonaws.com/" + path;
+		final String uri = "http://" + bucketName + ".s3.amazonaws.com/" + URLEncoder.encode(path);
 
 		return httpClient.execute(new HttpGet(uri), res -> {
 			return handler.handleResponse(res.getEntity().getContent());
